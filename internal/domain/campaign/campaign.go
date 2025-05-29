5d18bdc3315d3473b402189f8af4775e5be2450c
Sara Albuquerque <saraalbuquerque@live.com>
2025-05-28 23:22:42 -0300

    feat: create constructor newcampaign

--- internal/domain/campaign/campaign.go
+++ internal/domain/campaign/campaign.go
@@ -12,3 +12,20 @@ type Campaign struct {
 	Content   string
 	Contacts  []Contact
 }
+
+func NewCampaign(name string, content string, emails []string) *Campaign {
+	
+	contacts := make([]Contact, len(emails))
+	for index, email := range emails {
+		contacts[index].Email = email
+
+	}
+	
+	return &Campaign {
+		ID: "1",
+		Name: name,
+		Content: content,
+		CreatedOn: time.Now(),
+		Contacts: contacts,
+	}
+}
