40b9585eafc54acf62d57a461232bf663aff2e99
Sara Albuquerque <saraalbuquerque@live.com>
2025-05-29 22:27:20 -0300

    test: add unit test for NewCampaign function

--- internal/domain/campaign/campaign.go
+++ internal/domain/campaign/campaign.go
@@ -14,18 +14,18 @@ type Campaign struct {
 }
 
 func NewCampaign(name string, content string, emails []string) *Campaign {
-	
+
 	contacts := make([]Contact, len(emails))
 	for index, email := range emails {
 		contacts[index].Email = email
 
 	}
-	
-	return &Campaign {
-		ID: "1",
-		Name: name,
-		Content: content,
+
+	return &Campaign{
+		ID:        name,
+		Name:      "1",
+		Content:   content,
 		CreatedOn: time.Now(),
-		Contacts: contacts,
+		Contacts:  contacts,
 	}
 }
---
+++ internal/domain/campaign/campaing_test.go
@@ -0,0 +1,22 @@
+package campaign
+
+import "testing"
+
+func TestNewCampaign(t *testing.T) {
+	name := "Campaing x"
+	content := "Body"
+	contacts := []string{"email1@e.com", "email2@e.com"}
+
+	campaign := NewCampaign(name, content, contacts)
+
+	if campaign.ID != "1" {
+		t.Errorf("Expected campaign id to be 1, but got %s", campaign.ID)
+	} else if campaign.Name != name {
+		t.Errorf("Expected campaign name to be %s, but got %s", name, campaign.Name)
+	} else if campaign.Content != content {
+		t.Errorf("Expected campaign content to be %s, but got %s", content, campaign.Content)
+	} else if  len(campaign.Contacts) != len(contacts) {
+		t.Errorf("Expected campaign contacts to be %d, but got %d", len(contacts), len(campaign.Contacts))
+	}
+
+}
