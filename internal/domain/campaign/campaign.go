84565a654284c65621cce69b5d24e9eca1a0a3a1
Sara Albuquerque <saraalbuquerque@live.com>
2025-06-01 18:32:34 -0300

    fix: update NewCampaign to generate a unique ID using xid

--- internal/domain/campaign/campaign.go
+++ internal/domain/campaign/campaign.go
@@ -1,6 +1,10 @@
 package campaign
 
-import "time"
+import (
+	"time"
+
+	"github.com/rs/xid"
+)
 
 type Contact struct {
 	Email string
@@ -22,7 +26,7 @@ func NewCampaign(name string, content string, emails []string) *Campaign {
 	}
 
 	return &Campaign{
-		ID:        "1",
+		ID:        xid.New().String(),
 		Name:      name,
 		Content:   content,
 		CreatedOn: time.Now(),
--- internal/domain/campaign/campaing_test.go
+++ internal/domain/campaign/campaing_test.go
@@ -2,11 +2,12 @@ package campaign
 
 import (
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
 )
 
-func TestNewCampaign(t *testing.T) {
+func Test_NewCampaign_CreateCampaign(t *testing.T) {
 	//Arrange
 	assert := assert.New(t)
 	name := "Campaing x"
@@ -21,13 +22,38 @@ func TestNewCampaign(t *testing.T) {
 	campaign2 :=  NewCampaign(nome, conteudo, contato)
 	
 	//Assert
+	println(campaign.ID)	
 	assert.Equal(campaign.ID, "1")
 	assert.Equal(campaign.Name, name)
 	assert.Equal(campaign.Content, content)
 	assert.Equal(len(campaign.Contacts), len(contacts))
+
 	assert.Equal(campaign2.ID, "1")
 	assert.Equal(campaign2.Name, nome)
 	assert.Equal(campaign2.Content, conteudo)
 	assert.Equal(len(campaign2.Contacts), len(contato))
 
+}
+
+func Test_NewCampaign_IDisNotNill(t *testing.T) {
+	assert := assert.New(t)
+	name := "Campaing x"
+	content := "Body"
+	contacts := []string{"email1@e.com", "email2@e.com"}
+
+	campaign := NewCampaign(name, content, contacts)
+
+	assert.NotNil(campaign.ID)
+}
+
+func Test_NewCampaign_CreatedOnIsNotNill(t *testing.T) {
+	assert := assert.New(t)
+	name := "Campaing x"
+	content := "Body"
+	contacts := []string{"email1@e.com", "email2@e.com"}
+	now := time.Now().Add(-time.Minute)
+
+	campaign := NewCampaign(name, content, contacts)
+
+	assert.Greater(campaign.CreatedOn, now)
 }
