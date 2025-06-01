1d0300faf25ec9ebdfc164caa283918636e9d5fd
Sara Albuquerque <saraalbuquerque@live.com>
2025-06-01 20:53:34 -0300

    test: add service test unit tests for campaign creation and saving functionality

---
+++ internal/domain/campaign/service_test.go
@@ -0,0 +1,58 @@
+package campaign
+
+import (
+	"emailn/internal/contract"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+)
+
+type repositoryMock struct {
+	mock.Mock
+}
+
+func (r *repositoryMock) Save(campaign *Campaign) error {
+	args := r.Called(campaign)
+	return args.Error(0)
+}
+
+func Test_Create_Campaign(t *testing.T) {
+	assert := assert.New(t)
+	service := Service{}
+	newCampaign := contract.NewCampaign{
+		Name:    "Teste Y",
+		Content: "Body",
+		Emails:  []string{"teste1@test.com"},
+	}
+
+	id, err := service.Create(newCampaign)
+
+	assert.NotNil(id)
+	assert.Nil(err)
+
+}
+
+func Test_Create_SaveCampaign(t *testing.T) {
+	newCampaign := contract.NewCampaign{
+		Name:    "Teste Y",
+		Content: "Body",
+		Emails:  []string{"teste1@test.com"},
+	}
+	repositoryMock := new(repositoryMock)
+	repositoryMock.On("Save", mock.MatchedBy(func(campaign *Campaign) bool {
+		if campaign.Name != newCampaign.Name ||
+			campaign.Content != newCampaign.Content ||
+			len(campaign.Contacts) != len(newCampaign.Emails) {
+			return false
+		}
+
+		return true
+	})).Return(nil)
+	service := Service{Repository: repositoryMock}
+
+	service.Create(newCampaign)
+
+	repositoryMock.AssertExpectations(t)
+
+}
