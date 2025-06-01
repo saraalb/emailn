fe5a82557afd8f8cc2581b8ffc93a88a0faaea4d
Sara Albuquerque <saraalbuquerque@live.com>
2025-06-01 18:51:08 -0300

    test: rename TestNewCampaign to Test_NewCampaign_CreateCampaign and remove hardcoded ID assertion and add additional tests for NewCampaign ID and CreatedOn fields

--- internal/domain/campaign/campaing_test.go
+++ internal/domain/campaign/campaing_test.go
@@ -13,25 +13,15 @@ func Test_NewCampaign_CreateCampaign(t *testing.T) {
 	name := "Campaing x"
 	content := "Body"
 	contacts := []string{"email1@e.com", "email2@e.com"}
-	nome := "nome"
-	conteudo := "conteudo"
-	contato := []string{"contato1@e.com", "contato2@e.com"}
 
 	//Action
 	campaign := NewCampaign(name, content, contacts)
-	campaign2 :=  NewCampaign(nome, conteudo, contato)
 	
 	//Assert
 	println(campaign.ID)	
-	assert.Equal(campaign.ID, "1")
 	assert.Equal(campaign.Name, name)
 	assert.Equal(campaign.Content, content)
 	assert.Equal(len(campaign.Contacts), len(contacts))
-
-	assert.Equal(campaign2.ID, "1")
-	assert.Equal(campaign2.Name, nome)
-	assert.Equal(campaign2.Content, conteudo)
-	assert.Equal(len(campaign2.Contacts), len(contato))
 
 }
 
@@ -52,7 +42,7 @@ func Test_NewCampaign_CreatedOnIsNotNill(t *testing.T) {
 	content := "Body"
 	contacts := []string{"email1@e.com", "email2@e.com"}
 	now := time.Now().Add(-time.Minute)
-
+	
 	campaign := NewCampaign(name, content, contacts)
 
 	assert.Greater(campaign.CreatedOn, now)
