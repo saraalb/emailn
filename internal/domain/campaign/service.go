9f8075c5c180d7ce4a1168fe5dfd086687846551
Sara Albuquerque <saraalbuquerque@live.com>
2025-06-01 21:50:07 -0300

    fix service_teste and service

--- internal/domain/campaign/service.go
+++ internal/domain/campaign/service.go
@@ -2,6 +2,7 @@ package campaign
 
 import (
 	"emailn/internal/contract"
+	internalerrors "emailn/internal/internal-errors"
 )
 
 type Service struct {
@@ -10,9 +11,14 @@ type Service struct {
 
 func (s *Service) Create(newCampaign contract.NewCampaign) (string, error) {
 
-	campaign, _ := NewCampaign(newCampaign.Name, newCampaign.Content, newCampaign.Emails)
-	s.Repository.Save(campaign)
+	campaign, err := NewCampaign(newCampaign.Name, newCampaign.Content, newCampaign.Emails)
+	if err != nil {
+		return "", err
+	}
+	err = s.Repository.Save(campaign)
+	if err != nil {
+		return "", internalerrors.ErrInternal
+	}
 
 	return campaign.ID, nil
-
 }
--- internal/domain/campaign/service_test.go
+++ internal/domain/campaign/service_test.go
@@ -2,6 +2,8 @@ package campaign
 
 import (
 	"emailn/internal/contract"
+	internalerrors "emailn/internal/internal-errors"
+	"errors"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -38,6 +40,14 @@ func Test_Create_Campaign(t *testing.T) {
 	assert.Nil(err)
 }
 
+func Test_Create_ValidateDomainError(t *testing.T) {
+	assert := assert.New(t)
+
+	_, err := service.Create(contract.NewCampaign{})
+
+	assert.False(errors.Is(internalerrors.ErrInternal, err))
+}
+
 func Test_Create_SaveCampaign(t *testing.T) {
 	repositoryMock := new(repositoryMock)
 	repositoryMock.On("Save", mock.MatchedBy(func(campaign *Campaign) bool {
@@ -54,4 +64,15 @@ func Test_Create_SaveCampaign(t *testing.T) {
 	service.Create(newCampaign)
 
 	repositoryMock.AssertExpectations(t)
+}
+
+func Test_Create_ValidateRepositorySave(t *testing.T) {
+	assert := assert.New(t)
+	repositoryMock := new(repositoryMock)
+	repositoryMock.On("Save", mock.Anything).Return(errors.New("error to save on database"))
+	service.Repository = repositoryMock
+
+	_, err := service.Create(newCampaign)
+
+	assert.True(errors.Is(internalerrors.ErrInternal, err))
 }
