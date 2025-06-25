580d6f26c789716ce9d93abe4a9cb22741d68b2c
Sara Albuquerque <saraalbuquerque@live.com>
2025-06-25 20:47:53 -0300

    fix: add validation tags to Campaign struct fields

--- internal/domain/campaign/campaign.go
+++ internal/domain/campaign/campaign.go
@@ -8,14 +8,15 @@ import (
 )
 
 type Contact struct {
-	Email string
+	Email string `validate:"email"`
 }
+
 type Campaign struct {
-	ID        string
-	Name      string
-	CreatedOn time.Time
-	Content   string
-	Contacts  []Contact
+	ID        string    `validate:"required"`
+	Name      string    `validate:"min=5,max=24"`
+	CreatedOn time.Time `validate:"required"`
+	Content   string    `validate:"min=5,max=1024"`
+	Contacts  []Contact `validate:"min=1"`
 }
 
 func NewCampaign(name string, content string, emails []string) (*Campaign, error) {
